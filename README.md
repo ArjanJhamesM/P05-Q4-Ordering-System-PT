82f5fc2615be2223127d9b904763a38d0f6a9c35
ArjanJhamesM <arjanjhamesm@gmail.com>
2025-04-29 22:51:46 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# P05-Q4-Ordering-System-PT
+ Performance Tasks in Introduction to Web Design
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="ordering-layout">
+        <div class="products-panel">
+            <div class="products-layout">
+                <div class="products-list">
+                    <h1>Hello world! 1st</h1>
+                </div>
+
+                <div class="checkout-row">
+                    <div> <button id="checkout-button">Checkout</button> </div>
+                </div>
+            </div>
+            
+        </div>
+        <div class="costs-panel">
+            <h1>Hello world! 2nd</h1>
+        </div>
+    </div>
+    
+</body>
+</html>
+
+<?php
+
+?>
